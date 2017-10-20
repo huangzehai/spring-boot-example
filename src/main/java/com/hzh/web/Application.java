b79dd31a8154f1c2fcc9cf1a4cc7b7fcbcc96d4c
huangzehai <chaogeek@foxmail.com>
2017-10-20 18:43:41 +0800

    添加定时任务

--- src/main/java/com/hzh/web/Application.java
+++ src/main/java/com/hzh/web/Application.java
@@ -1,11 +1,11 @@
 package com.hzh.web;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
+@EnableScheduling
 public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
