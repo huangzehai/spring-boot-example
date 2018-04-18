5a2b748b810e5889cb0067bd13bac1f65bb2f8fb
huangzehai <chaogeek@foxmail.com>
2018-04-18 23:34:25 +0800

    Add BeanFactoryPostProcessor

--- src/main/java/com/hzh/web/processor/MyBeanPostProcessor.java
+++ src/main/java/com/hzh/web/processor/MyBeanPostProcessor.java
@@ -1,20 +1,22 @@
 package com.hzh.web.processor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyBeanPostProcessor implements BeanPostProcessor{
+@Slf4j
+public class MyBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("postProcessBeforeInitialization");
+        log.info("postProcessBeforeInitialization: {}", beanName);
         return bean;
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("postProcessAfterInitialization");
+        log.info("postProcessAfterInitialization: {}", beanName);
         return bean;
     }
 }
